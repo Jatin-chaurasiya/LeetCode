ff8c1270d28a19c34692e47bebef0cf7e3065243
Jatin Chaurasiya <159553125+Jatin-chaurasiya@users.noreply.github.com>
2026-03-15 22:55:06 +0530

    Time: 1 ms (87.49%), Space: 42.5 MB (73.73%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n < 1000){
+            return 0;
+        }
+        int ans = 0;
+        for(int k=1; ;k++){
+            int start = (int) Math.pow(10,3*k);
+            if(start > n) break;
+            ans += (n - start + 1)* k;
+        }
+        return ans;
+    }
+}
